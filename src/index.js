e07a0239dbba40678eb5106511e16c73f83afb4b
Asuka-Hongo <75460862+araaiko@users.noreply.github.com>
2021-06-22 00:07:46 +0900

    const変数の復習

--- src/index.js
+++ src/index.js
@@ -23,11 +23,26 @@
 // // let変数は再宣言不可能
 // let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
 // constは上書き不可
 // val3 = "const変数を上書き";
 
 // constは再宣言も不可
 // const val3 = "cosnt変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+// constで定義した配列は中身の編集や追加も可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
