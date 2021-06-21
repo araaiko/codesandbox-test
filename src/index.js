000b5caab6b51391c802538849d0b5fe255f5856
Asuka-Hongo <75460862+araaiko@users.noreply.github.com>
2021-06-21 23:39:32 +0900

    const変数の復習

--- src/index.js
+++ src/index.js
@@ -11,3 +11,23 @@
 // // var変数は再宣言可能
 // var val1 = "var変数を再宣言";
 // console.log(val1);
+
+// let
+// let val2 = "let変数";
+// console.log(val2);
+
+// // let変数は上書き可能
+// val2 = "let変数は上書き可能";
+// console.log(val2);
+
+// // let変数は再宣言不可能
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// constは上書き不可
+// val3 = "const変数を上書き";
+
+// constは再宣言も不可
+// const val3 = "cosnt変数を再宣言";
