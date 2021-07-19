54a41f8d8abe9c53883a088802db1281b625f320
Asuka-Hongo <75460862+araaiko@users.noreply.github.com>
2021-07-19 13:08:32 +0900

    セクション4完了

--- src/index.js
+++ src/index.js
@@ -182,3 +182,37 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ * if else の構文を一文で書ける
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// numの型式がナンバーだったら、金額のように3桁ごとにカンマを入れる。ナンバー以外だったら「数値を入力してください」を表示する
+//const num = '1300';
+// typeof 〜 で変数の型式を識別できる。
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！！' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 60));
+
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
+
+const flag1 = true;
+const flag2 = false;
+
+if (flag1 || flag2) {
+  console.log("1か2がtrueになります");
+}
